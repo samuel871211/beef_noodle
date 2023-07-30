055acc754a650a3d62942083503291aaeff4a847
shemira <samuel871211@gmail.com>
2023-07-30 17:01:00 +0800

    chore: firebase hosting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/test/beef_noodle'
+  output: 'export'
 }
 
 module.exports = nextConfig
